d988ba95dad8ebb87bdff362c8c4cddeebbd9259
Thami <thami.sadouk@outlook.fr>
2018-09-12 16:57:49 +0100

    Add form to add and edit contact

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -23,4 +26,40 @@ class BookController extends AbstractController
             'contacts' => $contacts
         ]);
     }
+
+     /**
+     * @Route("add", name="add_contact")
+     * @Route("{id}/edit", name="edit_contact")
+     * Can add or edit contact infos
+     */
+    public function form(Contact $contact = null, Request $request, ObjectManager $manager) 
+    {
+        // if there is no 'id' contact, then we create a new Contact object
+        if(!$contact) {
+            $contact = new Contact(); 
+        }
+
+        $form = $this->createForm(ContactType::class, $contact); 
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && ($form->isValid())) 
+        {   
+            // if the contact already exist we don't want to set a new Datetime
+            if(!$contact->getId()) {
+                $contact->setCreatedAt(new \Datetime()); 
+            } 
+
+            $manager->persist($contact); 
+            $manager->flush(); 
+
+            return $this->redirectToRoute('home'); 
+        } 
+
+        return $this->render('book/add.html.twig', [
+            'formContact' => $form->createView(), 
+            'editMode' => $contact->getId() !== null
+        ]); 
+    }
+
 }
